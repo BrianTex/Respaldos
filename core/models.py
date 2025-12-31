67dedde89ca5050e35f1f4beb0bccb66e11f9473
Brian <brianxd.tx@gmail.com>
2025-12-31 13:33:21 -0600

    bugs fixes

--- core/models.py
+++ core/models.py
@@ -7,9 +7,7 @@ class Server(models.Model):
     dominio_o_ip = models.CharField(max_length=255, unique=True, verbose_name="Dominio o IP")
     usuario_servidor = models.CharField(max_length=100, verbose_name="Usuario del Servidor")
     contrasena_bot = models.CharField(max_length=255, verbose_name="Contraseña del Bot")
-    # Opcional: Podríamos añadir una relación con el usuario que dio de alta el servidor
-    # owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='servers', null=True, blank=True)
-
+ 
     def __str__(self):
         return self.dominio_o_ip
 
