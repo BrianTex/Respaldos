446aef96d5b1e6b8e2cf4c1bb06f915955e49f9e
ManuelOlivo17 <josema173@hotmail.com>
2026-01-01 21:46:39 -0600

    Codigo ssh bot

--- core/utils.py
+++ core/utils.py
@@ -1,7 +1,7 @@
 from .models import Server
 from django.template import Template,Context
 from django.template.loader import render_to_string
-
+import paramiko
 def generar_script_personalizado(servidor, configuracion):
     contexto = {
         'servidor': servidor,
@@ -12,6 +12,28 @@ def generar_script_personalizado(servidor, configuracion):
     
     return script_final
 
+def automatizar_con_llave(Server, Configuracion, script):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    
+    ssh.connect(
+        Server.dominio_o_ip, 
+        username=Server.usuario_servidor, 
+        look_for_keys=True
+    )
+    sftp = ssh.open_sftp()
+    ruta_script = f"/home/{Server.usuario_servidor}/respaldo_bot.sh"
+    
+    with sftp.file(ruta_script, 'w') as f:
+        f.write(script)
+    sftp.close()
+    ssh.exec_command(f"chmod +x {ruta_script}")
+
+    linea_cron = f"{Configuracion.cron} {ruta_script}"
+    comando_cron = f'(crontab -l 2>/dev/null; echo "{linea_cron}") | crontab -'
+    ssh.exec_command(comando_cron)
+    ssh.close()
+
 def automatizarServidor(Server,Configuracion):
     script=generar_script_personalizado(Server,Configuracion)
-    
+    automatizar_con_llave(Server,Configuracion,script)
